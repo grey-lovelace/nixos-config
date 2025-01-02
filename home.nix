51ced5dacbdf78e5299a5905b30b80715fbf5742
Grey Lovelace <austin.grey.lovelace@gmail.com>
2025-01-02 09:09:31 -0600

    [A

--- home.nix
+++ home.nix
@@ -53,6 +53,7 @@
       switch-to-workspace-left=["<Control><Super>Left"];
       switch-to-workspace-right=["<Control><Super>Right"];
       toggle-message-tray=[];
+      show-screenshot-ui=["<Control><Super>s"]
     };
     "org/gnome/settings-daemon/plugins/media-keys" = {
       home = ["<Super>e"];
