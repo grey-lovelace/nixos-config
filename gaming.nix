c277b4998e3b895e9e20a7cb7894001ac19514c6
Grey Lovelace <austin.grey.lovelace@gmail.com>
2025-01-09 20:54:06 -0600

    Added heroic

--- gaming.nix
+++ gaming.nix
@@ -1,9 +1,15 @@
 { config, lib, pkgs, ... }:
 {
+    programs.gamemode.enable = true; # for performance mode
+    
     programs.steam = {
         enable = true;
         remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
         dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
         localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
     };
+
+    environment.systemPackages = with pkgs; [
+        heroic # install heroic launcher
+    ];
 }
