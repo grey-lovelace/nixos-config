4f1e715b02cbaa7fc3c3a2d707bb28a756ad285d
Grey Lovelace <austin.grey.lovelace@gmail.com>
2025-07-20 09:20:19 -0500

    Change disabled touch device to correct one for main screen

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -36,7 +36,7 @@ with lib; {
     # Disable touch capabilities
     services.udev.extraRules = ''
       ACTION!="remove", KERNEL=="event[0-9]*", ENV{ID_VENDOR_ID}=="4c4a", ENV{ID_MODEL_ID}=="4155", ENV{LIBINPUT_IGNORE_DEVICE}="1"
-      ACTION!="remove", KERNEL=="event[0-9]*", ENV{ID_VENDOR_ID}=="0911", ENV{ID_MODEL_ID}=="5288", ENV{LIBINPUT_IGNORE_DEVICE}="1"
+      ACTION!="remove", KERNEL=="event[0-9]*", ENV{ID_VENDOR_ID}=="27c6", ENV{ID_MODEL_ID}=="0113", ENV{LIBINPUT_IGNORE_DEVICE}="1"
     '';
   
     # Replace 'left' with 'right' or 'inverted' as needed
