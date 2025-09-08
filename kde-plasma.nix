6f34419f537a99670adcb08cbabfa5363aea8f65
Grey Lovelace <austin.grey.lovelace@gmail.com>
2025-09-08 11:08:14 -0500

    kdePackages.xdg-desktop-portal-kde

--- kde-plasma.nix
+++ kde-plasma.nix
@@ -17,6 +17,7 @@
 
     environment.systemPackages = with pkgs; [
         kdePackages.krunner
+        kdePackages.xdg-desktop-portal-kde
         # KDE
         kdePackages.discover # Optional: Install if you use Flatpak or fwupd firmware update sevice
         kdePackages.kcalc # Calculator
