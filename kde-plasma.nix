ea35943e1176ba52fc40c5529a7544eb8941731d
Grey Lovelace <austin.grey.lovelace@gmail.com>
2025-09-08 10:57:06 -0500

    [A

--- kde-plasma.nix
+++ kde-plasma.nix
@@ -2,7 +2,6 @@
 
 { config, pkgs, ... }:
 {
-    # Enable the GNOME Desktop Environment.
     services.displayManager.sddm.enable = true;
     services.displayManager.sddm.wayland.enable = true;
     services.desktopManager.plasma6.enable = true;
@@ -16,7 +15,22 @@
         NIXOS_OZONE_WL =  "1";
     };
 
-    environment.systemPackages = [
-        pkgs.kdePackages.krunner
+    environment.systemPackages =  = with pkgs; [
+        kdePackages.krunner
+        # KDE
+        kdePackages.discover # Optional: Install if you use Flatpak or fwupd firmware update sevice
+        kdePackages.kcalc # Calculator
+        kdePackages.kcharselect # Tool to select and copy special characters from all installed fonts
+        kdePackages.kclock # Clock app
+        kdePackages.kcolorchooser # A small utility to select a color
+        kdePackages.kolourpaint # Easy-to-use paint program
+        kdePackages.ksystemlog # KDE SystemLog Application
+        kdePackages.sddm-kcm # Configuration module for SDDM
+        kdiff3 # Compares and merges 2 or 3 files or directories
+        kdePackages.isoimagewriter # Optional: Program to write hybrid ISO files onto USB disks
+        kdePackages.partitionmanager # Optional: Manage the disk devices, partitions and file systems on your computer
+        kdiff3 # Compares and merges 2 or 3 files or directories
+        wayland-utils # Wayland utilities
+        hardinfo2 # System information and benchmarks for Linux systems
     ];
 }
