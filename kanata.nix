4102c2f41533d2cec5b38d079fa98c4329298581
Grey Lovelace <austin.grey.lovelace@gmail.com>
2026-02-23 14:32:52 -0600

    Change delete key to f13

--- kanata.nix
+++ kanata.nix
@@ -64,7 +64,7 @@
             tabr C-pgdn
           )
           (deflayer base
-            @escvanilla  f1   f2   f3   f4   f5   f6   f7   f8   f9   f10  f11  f12  del
+            @escvanilla  f1   f2   f3   f4   f5   f6   f7   f8   f9   f10  f11  f12  f13
             grv  1    2    3    4    5    6    7    8    9    0    -    =    bspc
             tab  q    w    e    r    t    y    u    i    o    p    [    ]    \
             @escnumnav a    s    d    f    g    h    j    k    l    ;    '    ret
